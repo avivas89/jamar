803ca7ad606922410ce508f6dd2c886fe080719d
anaismiraira <mirairaanais@gmail.com>
2021-01-22 21:08:21 -0430

    responsive

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,43 @@
-$('.owl-carousel').owlCarousel({
+$('.sliders').owlCarousel({
     loop:true,
     items:1,
     margin:0,
     nav:false,
 })
+
+$('.products').owlCarousel({
+    loop:true,
+    items:4,
+    margin:10,
+    nav:false,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:3,
+            nav:false
+        },
+        1000:{
+            items:5,
+            nav:true,
+            loop:false
+        }
+    }
+})
+
+function postsCarousel() {
+        var checkWidth = jQuery(window).width();
+
+        if (checkWidth < 767) {
+            jQuery('.offer').owlCarousel({
+                items:1,
+                dots: true,
+                nav: false,
+            })
+        }
+    }
+    postsCarousel();
+    jQuery(window).resize(postsCarousel);
